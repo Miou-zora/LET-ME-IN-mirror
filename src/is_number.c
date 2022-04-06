242ce87caf575ee52441f4e461362c3b1f7ff0e8
Queng123 <quentin.brejoin@epitech.eu>
2022-04-06 19:49:56 +0200

    ADD : is_number file to check if a string is a number

---
+++ src/is_number.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** LET-ME-IN-mirror
+** File description:
+** is_number
+*/
+
+int is_str_nbr(char *str)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+        if (str[i] < '0' || str[i] > '9')
+            return (84);
+    return (0);
+}
