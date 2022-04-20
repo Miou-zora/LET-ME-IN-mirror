f723162572c43c8bca988b5b1482b980d6322bcc
lhay <lylian.hay@epitech.eu>
2022-04-20 18:54:16 +0200

    ADD : create function display path to display the path of the ants

--- src/display/display_path.c
+++ src/display/display_path.c
@@ -9,9 +9,19 @@
 
 void display_path(data_t *data, list_t **tab_node)
 {
-    char ***moves = get_all_path(tab_node[0], data->end, tab_node);
-    for (int i = 0; moves[i] != NULL; i++) {
-        for (int j = 0; moves[i][j] != NULL; j++)
-            my_printf("%s", moves[i][j]);
+    char ***all_paths = get_all_path(tab_node[0], data->end, tab_node);
+    int nbr_paths = get_len_paths(all_paths);
+    int **ants = get_ants(all_paths, nbr_paths, data);
+    int len_first = get_len_array(all_paths[0]) + count_int_array(ants[0]);
+
+    for (int i = 1; len_first >= i; i++) {
+        display_movement(ants, all_paths, i);
+        my_putchar('\n');
     }
+    for (int i = 0; all_paths[i] != NULL; i++) {
+        free(all_paths[i]);
+        free(ants[i]);
+    }
+    free(all_paths);
+    free(ants);
 }
