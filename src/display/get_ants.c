e89ca03d945bc8e29c032ed031e949c2dfd10959
lhay <lylian.hay@epitech.eu>
2022-04-20 18:53:04 +0200

    ADD : create function get ants Get the pos of the min and add the numbers of ants per path and return an int **

---
+++ src/display/get_ants.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2022
+** LET-ME-IN-mirror
+** File description:
+** check_error_file
+*/
+
+#include "lem_in.h"
+
+int **get_ants(char ***all_paths, int nbr_paths, data_t *data)
+{
+    int **ants = my_calloc((nbr_paths + 1), sizeof(*ants));
+    int pos_paths = 0;
+
+    for (int i = 0; nbr_paths != i; i++) {
+        ants[i] = my_calloc((data->nb_ants + 1), sizeof(*ants));
+    }
+    for (int i = 1; i <= data->nb_ants; i++) {
+        pos_paths = get_pos_paths(all_paths, ants, nbr_paths);
+        append_int_list(ants[pos_paths], i);
+    }
+    return (ants);
+}
