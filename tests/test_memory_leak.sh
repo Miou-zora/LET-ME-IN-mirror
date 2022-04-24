16764c849169fecd3db57d26fe3027f923d31c85
miou-zora <alexandre.franquet@epitech.eu>
2022-04-24 17:30:54 +0200

    FIX : change condition of the test script bash test_memory_leak.sh

--- tests/test_memory_leak.sh
+++ tests/test_memory_leak.sh
@@ -13,7 +13,7 @@ for i in $(find -type f -path "./tests/maps/*.map");
 do
     valgrind --error-exitcode=3 ./lem_in < $i
     return_value=$?
-    if (($return_value == 3)); then
+    if [ "$return_value" -eq "3" ]; then
         printf '\033[31m********memory leak found with this file********\n\n%36s\n\n******************push disable******************\n\033[0m' $i
     else
         printf '\033[32m********no memory leak found********\n************push enable*************\n\033[0m'
